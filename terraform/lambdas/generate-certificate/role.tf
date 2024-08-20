04df992d65524f464934e37dafd1d8bbc6bd3069
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-20 16:33:16 -0300

    feat: criação da role da lambda

---
+++ terraform/lambdas/generate-certificate/role.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "generate_certificates_role" {
+  name = "generate_certificates_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
