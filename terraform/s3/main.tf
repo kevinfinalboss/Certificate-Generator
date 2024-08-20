7e0b825c8d099932578fdb8b16ae1391e19da138
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-20 13:01:33 -0300

    feat: criaçcao bucket s3

---
+++ terraform/s3/main.tf
@@ -0,0 +1,49 @@
+resource "aws_s3_bucket" "kevindev_applications" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.kevindev_applications.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+  bucket = aws_s3_bucket.kevindev_applications.bucket
+
+  rule {
+    id = "move_to_infrequent_access"
+
+    filter {
+      prefix = ""
+    }
+
+    transition {
+      days          = var.lifecycle_transition_days
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = var.deep_archive_transition_days
+      storage_class = "DEEP_ARCHIVE"
+    }
+
+    expiration {
+      days = var.expiration_days
+    }
+
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.kevindev_applications.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
