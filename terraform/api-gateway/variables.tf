f2032e213907afb98f51121ceb72106100bfd43e
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-20 16:32:25 -0300

    feat: adicionado arn e nome da lambda

--- terraform/api-gateway/variables.tf
+++ terraform/api-gateway/variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "lambda_function_name" {
+  description = "O nome da função Lambda a ser invocada pelo API Gateway"
+  type        = string
+  default     = "generate-certificates"
+}
+
+variable "lambda_invoke_arn" {
+  description = "O ARN de invocação da função Lambda para o API Gateway"
+  type        = string
+  default     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:116981773635:function:generate-certificates/invocations"
+}
