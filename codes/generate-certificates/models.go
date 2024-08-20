52957efa6e23e11cc898c1cd76355a2ae303bcca
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-20 18:43:37 -0300

    refactor: refatorado models e implementado novo campo UUID

--- codes/generate-certificates/models.go
+++ codes/generate-certificates/models.go
@@ -1,13 +1,14 @@
 package main
 
 type CertificateData struct {
-	ParticipantName string
-	CourseName      string
-	TotalHours      string
-	StartDate       string
-	EndDate         string
-	DirectorName    string
-	CoordinatorName string
-	CertificateID   string
-	IssueDate       string
+	UUID            string `json:"UUID"`
+	ParticipantName string `json:"participant_name"`
+	CourseName      string `json:"course_name"`
+	TotalHours      string `json:"total_hours"`
+	StartDate       string `json:"start_date"`
+	EndDate         string `json:"end_date"`
+	DirectorName    string `json:"director_name"`
+	CoordinatorName string `json:"coordinator_name"`
+	CertificateID   string `json:"certificate_id"`
+	IssueDate       string `json:"issue_date"`
 }
