646c313b3ba8fc92bebd5b5fcdadc4eff32b49a2
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-20 16:31:11 -0300

    feat: criado models da lambda

---
+++ codes/generate-certificates/models.go
@@ -0,0 +1,13 @@
+package main
+
+type CertificateData struct {
+	ParticipantName string
+	CourseName      string
+	TotalHours      string
+	StartDate       string
+	EndDate         string
+	DirectorName    string
+	CoordinatorName string
+	CertificateID   string
+	IssueDate       string
+}
