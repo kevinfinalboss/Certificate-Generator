3655b1a948ee25f48f4ad92404ab957587a94341
Kevin the GOAT <kevinmg50@gmail.com>
2024-08-21 21:19:50 -0300

    feat: role

---
+++ terraform/iam/aurora-bot/role.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "svc_aurora_bot_role" {
+  name = "${var.user_name}_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
